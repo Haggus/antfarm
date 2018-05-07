6a748db7cef683f3a6563b586c8f9dd36677b1eb
Mateusz Mrowiec <matt.mrowiec@gmail.com>
2018-05-07 23:08:08 +0200

    Move custom definitions to separate file

--- init.el
+++ init.el
@@ -5,9 +5,16 @@
 
 (package-initialize)
 
+;; move custom defs out of init.el
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
+
+(add-to-list 'default-frame-alist '(font . "Fira Mono-12"))
+(set-face-attribute 'default t :font "Fira Mono-12")
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -27,21 +34,3 @@
   :ensure t
   :init
   (evil-mode))
-
-(add-to-list 'default-frame-alist '(font . "Fira Mono-12"))
-(set-face-attribute 'default t :font "Fira Mono-12")
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (zenburn-theme evil which-key use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
